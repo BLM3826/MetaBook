8b37c1494adc685544f1c12c458a80b32709f9fd
Bill Ballas <ballas@programmer.net>
2021-11-03 10:59:16 +0200

    progress in routes, comment the controllers

--- server/controllers/BlogPostController.js
+++ server/controllers/BlogPostController.js
@@ -1,18 +1,16 @@
-var restful = require('node-restful');
+// var restful = require("node-restful");
 
-module.exports = function (app, route) {
-    
-    // Setup the controller for REST.
-    var rest = restful.model(
-        'blogpost',
-        app.models.BlogPost
-    ).methods(['get', 'post', 'put', 'delete']);
+// module.exports = function (app, route) {
+//   // Setup the controller for REST.
+//   var rest = restful
+//     .model("blogpost", app.models.BlogPost)
+//     .methods(["get", "post", "put", "delete"]);
 
-    // Register this endpoint with the application.
-    rest.register(app, route);
+//   // Register this endpoint with the application.
+//   rest.register(app, route);
 
-    // Return middleware.
-    return function (req, res, next) {
-        next();
-    };
-};
+//   // Return middleware.
+//   return function (req, res, next) {
+//     next();
+//   };
+// };
--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,28 @@
-module.exports = {
-    '/blogpost': require('./controllers/BlogPostController')
+module.exports = function (app, express) {
+    
+    // Blog Post Routes
+    var blogpost = require('./controllers/BlogPostController');
+    app.get('/api/blogpost', blogpost.getAll);
+    app.get('/api/blogpost/:id', blogpost.getById);
+    app.post('/api/blogpost', blogpost.create);
+    app.put('/api/blogpost/:id', blogpost.update);
+    app.delete('/api/blogpost/:id', blogpost.delete);
+    
+    // User Routes
+    var user = require('./controllers/UserController');
+    app.post('/api/login', user.login);
+    app.post('/api/logout', user.logout);
+    app.post('/api/register', user.register);
+
+    // Other Routes
+    app.get('/api/', function (req, res) {
+        res.send('testAPI');
+    });
+
+    // All other routes should redirect to the index.html
+    // app.get('*', function (req, res) {
+    //     res.sendfile('./public/index.html');
+    // });
+
+    
 };
