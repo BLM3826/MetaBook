c38bc7b80fa7e0f2aae5d3b0bd8bc8488981a3d7
blm3826 <ballas@programmer.net>
2021-11-25 11:13:02 +0200

    login popup func created

--- client/src/header/header.js
+++ client/src/header/header.js
@@ -1,9 +1,37 @@
+/* eslint-disable no-console */
 import template from './header.html';
+import connectTemplate from './login/login.html';
 import './header.css';
+
+export class headerController {
+  constructor($mdDialog, $log) {
+    this.$mdDialog = $mdDialog;
+    this.$log = $log;
+  }
+
+  openConnect(ev) {
+    console.log('openConnectDialog');
+    this.$mdDialog
+      .show({
+        controller: () => {},
+        controllerAs: 'vm',
+        bindToController: true,
+        template: connectTemplate,
+        targetEvent: ev,
+        clickOutsideToClose: true,
+        hasBackdrop: true,
+      })
+      .catch((err) => {
+        this.$log.error(err);
+      });
+  }
+}
+
+headerController.$inject = ['$mdDialog', '$log'];
 
 const bindings = {
   text: '<',
   user: '<',
 };
 
-export default { template, bindings };
+export default { controller: headerController, template, bindings };
