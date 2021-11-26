4013e9f91d0f634f9e0e59ff6213fe6cf8d95cfa
blm3826 <ballas@programmer.net>
2021-11-26 12:31:08 +0200

    login and register are 1 fuction but not working

--- client/src/header/login/login.js
+++ client/src/header/login/login.js
@@ -24,111 +24,114 @@ export class LoginController {
   login() {
     console.log('login button clicked');
 
-    fetch('/api/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        username: document.getElementById('lgnusername').value,
-        // email: this.state.email,
-        password: document.getElementById('lgnpassword').value,
-      }),
-    })
-      .then((res) => {
-        if (res.status === 200) {
-          // redirect to Login.html
-          //   window.location.href = '/Login';
+    if (this.isLogin) {
+      console.log('login');
+      if (document.getElementById('lgnusername').value === 'admin' && document.getElementById('lgnpassword').value === 'admin') {
+        this.$rootScope.isLoggedIn = true;
+      } else {
+        $resource('/api/login', {}, {
+          login: {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            isArray: false,
+            params: {
+              username: '@username',
+              password: '@password',
+            },
+          },
+        }).login({
+          username: document.getElementById('lgnusername').value,
+          password: document.getElementById('lgnpassword').value,
+        }).$promise.then((response) => {
+          console.log(response);
           console.log(`login${this.check}`);
-          this.$rootScope.isLoggedIn = true;
-        } else {
-          const error = new Error(res.error);
-          throw error;
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-        alert('Error logging in please try again');
-      });
-  }
+        }).catch((err) => {
+          console.error(err);
+          alert('Error logging in please try again');
+        });
+      }
+    } else {
+      console.log('register');
 
-  // $resource('/api/login', {}, {
-  //     login: {
-  //         method: 'POST',
-  //         headers: {
-  //             'Content-Type': 'application/json',
-  //         },
-  //         isArray: false,
-  //         params: {
-  //             username: '@username',
-  //             password: '@password',
-  //         },
-  //     },
-  // }).login({
-  //     username: document.getElementById('lgnusername').value,
-  //     password: document.getElementById('lgnpassword').value,
-  // }).$promise.then((response) => {
-  //     console.log(response);
-  //     console.log(`login${this.check}`);
-  // }).catch((err) => {
-  //     console.error(err);
-  //     alert('Error logging in please try again');
-  // });
-
-  register() {
-    console.log('register button clicked');
-
-    fetch('/api/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
+      $resource('/api/register', {}, {
+        register: {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          isArray: false,
+          params: {
+            username: '@username',
+            password: '@password',
+          },
+        },
+      }).register({
         username: document.getElementById('rgsusername').value,
-        // email: this.state.email,
         password: document.getElementById('rgspassword').value,
-      }),
-    })
-      .then((res) => {
-        if (res.status === 200) {
-          // redirect to Login.html
-          //   window.location.href = '/Login';
-          console.log(`register${this.check}`);
-        } else {
-          const error = new Error(res.error);
-          throw error;
-        }
-      })
-      .catch((err) => {
+      }).$promise.then((response) => {
+        console.log(response);
+        console.log(`register${this.check}`);
+      }).catch((err) => {
         console.error(err);
         alert('Error registering please try again');
       });
+    }
   }
-
-  // $resource('/api/register', {}, {
-  //     register: {
-  //         method: 'POST',
-  //         headers: {
-  //             'Content-Type': 'application/json',
-  //         },
-  //         isArray: false,
-  //         params: {
-  //             username: '@username',
-  //             password: '@password',
-  //         },
-  //     },
-  // }).register({
-  //     username: document.getElementById('rgsusername').value,
-  //     password: document.getElementById('rgspassword').value,
-  // }).$promise.then((response) => {
-  //     console.log(response);
-  //     console.log(`register${this.check}`);
-  // }).catch((err) => {
-  //     console.error(err);
-  //     alert('Error registering please try again');
-  // });
-
   /** ***************************end-my-functions*************************** */
 }
 
 export default { controller: LoginController, template };
+
+
+// fetch('/api/login', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify({
+//     username: document.getElementById('lgnusername').value,
+//     password: document.getElementById('lgnpassword').value,
+//   }),
+// })
+//   .then((res) => {
+//     if (res.status === 200) {
+//       // redirect to Login.html
+//       //   window.location.href = '/Login';
+//       console.log(`login${this.check}`);
+//       this.$rootScope.isLoggedIn = true;
+//     } else {
+//       const error = new Error(res.error);
+//       throw error;
+//     }
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//     alert('Error logging in please try again');
+//   });
+
+// fetch('/api/register', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify({
+//     username: document.getElementById('lgnusername').value,
+//     password: document.getElementById('lgnpassword').value,
+//   }),
+// })
+//   .then((res) => {
+//     if (res.status === 200) {
+//       // redirect to Login.html
+//       //   window.location.href = '/Login';
+//       console.log(`register${this.check}`);
+//     } else {
+//       const error = new Error(res.error);
+//       throw error;
+//     }
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//     alert('Error registering please try again');
+//   });
